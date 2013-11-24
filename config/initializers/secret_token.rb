d95f347ff1cf20376e67c3326a5070f1f40e0995
mctyn64 <bneuman@unknown949426010a1e.att.net>
2013-11-24 11:11:22 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firsty::Application.config.secret_key_base = '007c64f80debdaefc606c84e00e7ad7b8a9dbc8705b400ba31526efc1a24516177764fb409830acf9f24852df315f279d54a7936229cb3f353cbfc7def815326'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Firsty::Application.config.session_store :cookie_store, key: '_firsty_session'
